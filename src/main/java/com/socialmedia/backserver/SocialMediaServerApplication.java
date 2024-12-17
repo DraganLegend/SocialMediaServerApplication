1ef04ca4cfdf63d4af16887432dff34a7093cad0
draganlegend <DraganLegend@protonmail.com>
2024-12-17 14:40:23 +0800

    update

--- src/main/java/com/socialmedia/backserver/SocialMediaServerApplication.java
+++ src/main/java/com/socialmedia/backserver/SocialMediaServerApplication.java
@@ -4,9 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class
-SocialMediaServerApplication {
-
+public class SocialMediaServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(SocialMediaServerApplication.class, args);
     }
